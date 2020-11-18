2bcc95068e30224f1ebb8421001c91031ea47efb
Ronald PHLYPO AMATE <ronald.phlypo@grenoble-inp.fr>
2020-11-18 10:23:57 +0100

    lognormal estimator and realisations coded using scipy

--- TODO.md
+++ TODO.md
@@ -1,8 +1,8 @@
 # TODO-list
 
-- [X] creating the four different plaids: Amb, nAmbLeft, nAmbRight, nAmbCoh [Ronald]
+- [x] creating the four different plaids: Amb, nAmbLeft, nAmbRight, nAmbCoh [Ronald]
 - [ ] getting the experiment structure set: familiarising, estimation, testing [Juliette]
-- [ ] coding the estimator for the log-normal waiting times => transition matrix ? [Ronald]
+- [x] coding the estimator for the log-normal waiting times => transition matrix ? [Ronald]
 - [ ] handling the triggers over the parallel port [Juliette]
 - [ ] setting up the link with Eyelink (pylink) [Ronald]
 - [ ] recording the crucial parameters to replay the exact same experiment [Juliette]
--- functions.py
+++ functions.py
@@ -1,11 +1,30 @@
-from math import cos, sin
+from scipy.stats import lognorm
+import numpy as np
 
-def intensity_contrast(alpha, mu, c, dutycyle):
-    surf = [dutycyle ** 2, (1 - dutycycle) ** 2]
 
-    contrast = 2 * (alpha * c[0] + (1 - alpha) * c[1])
+def loglikelihood_lognormal(waiting_times):
+    """ compute the most likely parameters for the log-normal law, given waiting times
+    Arguments
+    waiting_times   : array of waiting times in the process
 
-    intensity = alpha * mu[0] + (1-alpha) * mu[1]
-    intensity += (surf[0] - surf[1]) * contrast / 2
+    Returns
+    mu      : mean of the lognormal variable
+    sigma   : standard deviation of the lognormal variable
+    """
+    sigma, loc, scale = lognorm.fit(waiting_times, loc=0)
+    mu = np.log(scale)
+    return mu, sigma
 
-    return intensity, contrast
+
+def draw_next_waiting_time(mu, sigma):
+    """ compute the waiting time until the next event, given lognormal distribution
+    Arguments
+    mu      : mean of the lognormal variable
+    sigma   : standard deviation of the lognormal variable
+
+    Returns
+    t       : a single sample, specifying waiting time until the next event (flip)
+    """
+
+    X = lognorm(s=sigma, loc=0, scale=np.exp(mu))
+    return X.rvs()
