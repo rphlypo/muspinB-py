ec95c2286e995ab08ced8ddd0fef462d6b99303d
Juliette <juliette.lenouvel@gmail.com>
2020-11-19 16:41:02 +0100

    add utils.py test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,12 +1,20 @@
-import utils as f
+import utils
 import numpy as np
+from psychopy.hardware import keyboard
 
-def test_loglikelihood_normal(k=1):
+def test_wait_keypress( wait):
+    kb = keyboard.Keyboard()
+    utils.wait_keypress( kb, wait)
+
+
+def test_loglikelihood_normal( k=1):
     mu0, sigma0 = 3, 2
-    wt = np.array([f.draw_next_waiting_time(mu0, sigma0) for _ in range(1000)])
-    mu, sigma = f.loglikelihood_lognormal(wt)
-    np.testing.assert_almost_equal(mu, mu0, decimal=k)   # check whether equal on the first digit
-    np.testing.assert_almost_equal(sigma, sigma0, decimal=k)   # check whether equal on the first digit
+    wt = np.array([utils.draw_next_waiting_time( mu0, sigma0) for _ in range( 1000)])
+    mu, sigma = utils.loglikelihood_lognormal( wt)
+    np.testing.assert_almost_equal( mu, mu0, decimal=k)   # check whether equal on the first digit
+    np.testing.assert_almost_equal( sigma, sigma0, decimal=k)   # check whether equal on the first digit
 
 if __name__ == "__main__":
     test_loglikelihood_normal()
+    print( 'Press space please')
+    test_wait_keypress( 5)
