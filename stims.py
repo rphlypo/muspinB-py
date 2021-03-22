9d9d263b307e65630821396eec04d1ee091a97cc
Ronald PHLYPO AMATE <ronald.phlypo@grenoble-inp.fr>
2021-03-22 22:06:40 +0100

    added function to get arbitrary speed vector of grating as a function of the orientation

--- stims.py
+++ stims.py
@@ -1,11 +1,12 @@
+from numpy.core.arrayprint import str_format
 from psychopy import visual
 from psychopy.tools.monitorunittools import deg2pix
-from math import sin, cos, pi, tan, atan
+from math import sin, cos, pi, tan, atan, sqrt
 import numpy as np
 import random
 
 
-def createPlaids(win, alpha, I0=1, normalise=True, **plaid_params):  # use only for coherent
+def createPlaids(win, alpha, normalise=False, **plaid_params):  # use only for coherent
     """ plaids are created in true transparency (multiplicative)
     :Parameters:
 
@@ -39,31 +40,30 @@ def createPlaids(win, alpha, I0=1, normalise=True, **plaid_params):  # use only 
 
     sf = plaid_params['sf']
     plaid_params.pop('sf')
-    dc = int(plaid_params['tex'][3:]) / 100
-    plaid_params.pop('tex')
+    dc = plaid_params['dc']
+    plaid_params.pop('dc')
     plaid_params['opacity'] = 1
     plaid_params['blendmode'] = 'add'
     res = plaid_params['texRes']
     angle = plaid_params['ori'] * pi / 180
     plaid_params.pop('ori')
-    plaid_params['contrast'] = I0
+    I0 = plaid_params['I0']
+    plaid_params['contrast'] = 1
+    plaid_params.pop('I0')
 
     g = np.mgrid[:res, :res] / res
     Y, X = g[0], g[1]
-    h = (1-dc) / 2
 
     # this represents a square pattern that can be used as a texture (X-like pattern)
-    grating_r = np.where(np.logical_or(
-                    np.logical_and(Y>1-h-X, Y<1+h-X),
-                    np.logical_or(Y>2-h-X, Y<h-X)), 1, alpha[0])
-    grating_l = np.where(np.logical_or(
-                    np.logical_and(Y>-h+X, Y<h+X),
-                    np.logical_or(Y>1-h+X, Y<-1+h+X)), 1, alpha[1])
+    grating_r = np.where(np.cos(2*np.pi*(Y+X)) > np.cos(2*np.pi*dc/2), alpha[0], 1)
+    grating_l = np.where(np.cos(2*np.pi*(Y-X)) > np.cos(2*np.pi*dc/2), alpha[1], 1)
 
-    transp_mean = (1 - dc) ** 2 * alpha[0] * alpha[1] + dc ** 2 + dc * (1 - dc) * (alpha[0] + alpha[1])
+    # the grating is the background illumination I0 multiplied by the absorption coefficients
     grating = I0 * grating_r * grating_l
     if normalise:
-        grating = (grating - transp_mean) / (1 - alpha[0] * alpha[1])
+        print(alpha, dc)
+        grating = __normalise(grating, alpha, dc, normalisation='center')
+        print(__normalise(1, alpha, dc))
     
     bkg = visual.Circle(win=win, size=12.7, lineWidth=0, fillColor=bkgcol, autoDraw=False)
     grating = visual.GratingStim(win=win, tex=grating, sf=(sf*cos(angle), sf*sin(angle)), **plaid_params)
@@ -71,21 +71,33 @@ def createPlaids(win, alpha, I0=1, normalise=True, **plaid_params):  # use only 
     return bkg, grating
 
 
-def createDots(win, coherence=0.5, nDots=100):
+def __normalise(x, alpha, dc, normalisation='zscore'):
+    transp_mean = (1 - dc) ** 2 * alpha[0] * alpha[1] + dc ** 2 + dc * (1 - dc) * (alpha[0] + alpha[1])
+    if normalisation == 'centering':
+        return x - transp_mean
+    elif normalisation == 'zscore':
+        return (x - transp_mean) / (1 - alpha[0] * alpha[1])
+    else:
+        return x
+
+
+def createDots(win, alpha, dc, coherence=0.5, nDots=100, I0=1):
     ''' Create RDK within a circular aperture to overlay on the plaids
     :Parameters:
 
-    dir: direction in degrees, 0° is left-right, 90° is down-up
+    dir: direction in degrees, 0° is left-right, -90° is down-up
 
     '''
     dp = dict(fieldShape='circle',
               fieldSize=12.7,
               signalDots='same',
               noiseDots='walk',
+              coherence=coherence,
+              nDots=nDots,
               dotSize=3,
-              dotLife=30)
-    dp['nDots'] = nDots
-    dots = visual.DotStim(win, opacity=0.7, units='deg', **dp)
+              dotLife=10)
+    color = I0  # __normalise(I0, alpha, dc, normalisation='center')
+    dots = visual.DotStim(win, units='deg', contrast=I0, **dp)
     return dots
 
 
@@ -97,7 +109,7 @@ def fixation(win):
     return bkg, circle, fix_point
 
 
-def plaids(win, alpha, ori, sf, dc, **kwargs):
+def plaids(win, alpha, sf, dc, ori, I0=1, normalise=False, **kwargs):
     """ create different moving plaids for different conditions ambiguous, coherent, transparent-left, transparent-right 
 
     :Parameters:
@@ -117,10 +129,11 @@ def plaids(win, alpha, ori, sf, dc, **kwargs):
     dc: float
         duty cycle of the gratings
     """
-    texture = 'sqr{:2d}'.format(dc)
 
     plaid_params = dict(
-        tex=texture,
+        dc=dc,
+        I0=I0,
+        normalise=normalise,
         mask='circle',
         blendmode='add', 
         sf=sf,
@@ -132,9 +145,6 @@ def plaids(win, alpha, ori, sf, dc, **kwargs):
         texRes=1024,
         autoDraw=False)  # color=win.color,
 
-    circle = visual.Circle(win, size=2.5, lineWidth=0, lineColor=win.color, fillColor=win.color, autoDraw=False)
-    fix_point = visual.Circle(win, size=2, units='pix', lineWidth=0, lineColor="black", fillColor="black", autoDraw=False)
-
     plaid_stims = dict(
         amb=createPlaids(win, [alpha['amb'], alpha['amb']], **plaid_params),
         transpL=createPlaids(win, [alpha['transp'], 1-alpha['transp']], **plaid_params),
@@ -142,10 +152,13 @@ def plaids(win, alpha, ori, sf, dc, **kwargs):
         coh=createPlaids(win, [alpha['coh'], alpha['coh']], **plaid_params)
         )
 
-    for stim in plaid_stims:
-        plaid_stims[stim] = (*plaid_stims[stim], circle, fix_point)
-
     return plaid_stims
+
+
+def fixation_disk(win):
+    circle = visual.Circle(win, size=2.5, lineWidth=0, lineColor=win.color, fillColor=win.color, autoDraw=False)
+    fix_point = visual.Circle(win, size=3, units='pix', lineWidth=0, lineColor="black", fillColor="black", autoDraw=False)
+    return circle, fix_point
 
 
 def sample_next_stim(current_stim=None, tm=None):  # TODO: implement transition between plaid_stims
@@ -166,8 +179,9 @@ def get_velocity_vector(vel=1, ori=0, sf=1, vel_units='cycle',**kwargs):
     :param ori: float
                 angle in degrees of the wavefront with respect to the vertical axes
     """
-    ori_rad = ori*pi/180
-    vel_rad = vel*pi/180
+    ori_rad = ori*pi/180  # deg --> rad
+    vel_rad = vel*pi/180  # deg --> rad
+
     if vel_units == 'cycle':
         dots_vel = [atan(tan(vel_rad)/cos(ori_rad))*180/pi*cos(ori_rad)*sf,
                     atan(tan(vel_rad)/sin(ori_rad))*180/pi*sin(ori_rad)*sf]
@@ -175,3 +189,16 @@ def get_velocity_vector(vel=1, ori=0, sf=1, vel_units='cycle',**kwargs):
         dots_vel = [atan(tan(vel_rad)/cos(ori_rad))*180/pi,
                     atan(tan(vel_rad)/sin(ori_rad))*180/pi]
     return dots_vel
+
+
+def _deg2rad(d):
+    return d / 180 * pi
+
+
+def _rad2deg(r):
+    return r / pi * 180
+
+
+def get_angular_velocity(target_ori, vel_deg=1, stim_ori=0):
+    Delta_theta = _deg2rad(target_ori - stim_ori)  # Delta_theta in radians
+    return _rad2deg(atan(tan(_deg2rad(vel_deg)) * sqrt(1 + tan(Delta_theta) ** 2)))
