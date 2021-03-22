c91993a6d11a4b30274b42f0fa0b057198093802
Ronald PHLYPO AMATE <ronald.phlypo@grenoble-inp.fr>
2021-03-22 22:07:37 +0100

    some more playing around with different velocities (direction and speed)

--- plaid_and_dots.py
+++ plaid_and_dots.py
@@ -5,11 +5,12 @@ import utils
 import stims
 import expsetup
 import numpy as np
-from math import sin
 
 
-# settuing up the plaids
+# setting up the plaids
 init = utils.load_init('./config/init.yaml') 
+# get the stim parameters
+stim_params = init['experiment']['stim']
 # launch a win instance to intercept the keypresses 
 # so that they are not sent to the console
 # set-up the Display and monitor
@@ -26,19 +27,36 @@ win = visual.Window(
     allowGUI=False,  
     waitBlanking=False, 
     winType='pyglet')  # for iohub to function well
-# getting the stims and its parameters
-velocity = np.array(
-    [0,
-     -stims.get_velocity_vector(**init['experiment']['stim'])[1]]
-    )
-plaid_stims = stims.plaids(win, **init['experiment']['stim'])
+
+
+# velocity is upward, computed from the oblique velocity vector
+# this velocity is in periods/s 
+# the velocity is thus relatif to the vertical period which is 1/(sin(theta)*sf) [deg]
+sf_y = stim_params['sf'] * np.sin(stims._deg2rad(stim_params['ori']))
+v_y = stims.get_angular_velocity(-90, stim_params['vel'], stim_params['ori'])
+velocity = np.array([0, -v_y * sf_y])
+
+# the actual plaids stimulus
+plaid_stims = stims.plaids(win, **stim_params)
 plaid = plaid_stims['amb']
-for s in plaid[:-3]: s.autoDraw = True
-dots = stims.createDots(win)
-dots.coherence = .7
-dots.dir = -90
+for s in plaid: s.autoDraw = True
+
+# the superimposed RDK
+dots = stims.createDots(
+    win,
+    [stim_params['alpha']['amb']]*2, 
+    stim_params['dc'],
+    nDots=100,
+    I0=stim_params['I0'])
+dots.coherence = 1
+dots.dir = 15
+dots_velocity = lambda x: stims.get_angular_velocity(
+    x, stim_params['vel'], stim_params['ori'])
 dots.autoDraw = True
-for s in plaid[-3:]: s.autoDraw = True
+
+# the fixation disk
+fix_disk = stims.fixation_disk(win)
+for s in fix_disk: s.autoDraw = True
 
 # Start the ioHub process. 'io' can now be used during the
 # experiment to access iohub devices and read iohub device events
@@ -49,20 +67,31 @@ print('Press [SPACE] to continue... ')
 percepts.waitKeyPress(io, key=' ', timeout=10)
 
 all_percepts = []
-trial_timer = clock.CountdownTimer(start=10)
+trial_timer = clock.CountdownTimer(start=20)
 
 pb = percepts.get_percept_report(io, clear=True)
 current_percept = pb[-1]
 
 new_time = trial_timer.getTime()
+current_dir = 0
 
 while trial_timer.getTime()>0:
     old_time = new_time
     new_time = trial_timer.getTime()
-    if dots.dir in [-90, 90]:
-        dots_vel = stims.get_velocity_vector(vel_units='deg', **init['experiment']['stim'])[1]
-    elif dots.dir in [0, 180]:
-        dots_vel = stims.get_velocity_vector(vel_units='deg', **init['experiment']['stim'])[0]
+    
+    if new_time > 15:
+        dots.coherence = 0
+    elif new_time > 10:
+        dots.coherence = 1
+        dots.dir = 15
+    elif new_time > 5:
+        dots.dir = -90
+    else:
+        dots.dir = -165
+    if dots.dir != current_dir:
+        dots_vel = dots_velocity(dots.dir)
+    current_dir = dots.dir
+
     dots.speed =  dots_vel * (new_time - old_time)  # speed in °/frame
     plaid[1].phase += velocity * (new_time - old_time)
     win.flip()
