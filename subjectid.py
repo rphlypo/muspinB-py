8ef6652041e7a2d0748b06231b1a599cc8209169
Ronald PHLYPO AMATE <ronald.phlypo@grenoble-inp.fr>
2022-07-05 16:26:47 +0200

    added 'copy to clipboard' functionality to subjectid.py

--- subjectid.py
+++ subjectid.py
@@ -1,6 +1,9 @@
 #%%
 # attention, ne pas mettre d'accents
 import hashlib
+import pandas as pd
+
+
 
 subject = dict()
 subject['prenom'] = input('Prénom sujet : ')
@@ -14,4 +17,9 @@ h.update('{}{}{}{}'.format(subject['prenom'].lower(),
                             subject['nom'].lower(),
                             subject['date'],
                             subject['genre'].lower()).encode())
-print(h.hexdigest())
+
+subject_id = h.hexdigest()[:16]
+df = pd.DataFrame([subject_id])
+df.to_clipboard(index=False,header=False)
+# subprocess.run('xclip', '-selection "clipboard"', universal_newlines=True, input=subject_id)
+print("subject id {} succesfully copied to clipboard\nuse CTRL+V to paste the id in your favourite application".format(subject_id))
